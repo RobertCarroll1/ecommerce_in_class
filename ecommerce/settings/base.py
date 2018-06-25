b68ef8781a4e4ea443cfe54b85a3082b1fbba967
Haley Schafer <hschafer@umich.edu>
2018-06-25 11:25:03 +0000

    storage bug fix

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -70,13 +71,6 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
