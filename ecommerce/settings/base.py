39aeecb8ef2ff45ece71382f8702ed4d0a1fed4a
Haley Schafer <hschafer@umich.edu>
2018-06-25 11:13:20 +0000

    Travis Configuration

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-haleyschafer.c9users.io']
+ALLOWED_HOSTS = ['ecommerce-haleyschafer.c9users.io', 'hschafer-ecommerce.herokuapp.com']
 
 
 # Application definition
