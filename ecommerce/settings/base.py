9b1271f77f20b2a48b0c1ea36e519c2fc2f1733b
Robert Carroll <robertcarrollonline@gmail.com>
2018-06-26 09:19:31 +0000

    initial commit

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-haleyschafer.c9users.io', 'hschaferecommerce.herokuapp.com']
+ALLOWED_HOSTS = ['ecommerce-in-class2-robertcarroll.c9users.io']
 
 
 # Application definition
