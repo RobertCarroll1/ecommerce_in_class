13e87ac2284acca4605df20e910e797073ecff04
Robert Carroll <robertcarrollonline@gmail.com>
2018-06-26 09:28:03 +0000

    travis connected

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-in-class2-robertcarroll.c9users.io']
+ALLOWED_HOSTS = ['ecommerce-in-class2-robertcarroll.c9users.io','ecommerce-in-class-app.herokuapp.com']
 
 
 # Application definition
